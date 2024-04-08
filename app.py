a913d28db1c4c486f9d4de119522000073729bb7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-09 11:22:46 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('hello, world')
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "SELECT * FROM fighter;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
